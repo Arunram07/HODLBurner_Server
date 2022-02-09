243235e59b81443c8f1130d3986c2810e7d6e456
Arunram07 <arunram@zevocorp.in>
2022-02-09 11:07:15 +0530

    feat :chart

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 import express from "express";
+import path from "path";
 import initiateApp from "./utils/initiateWeb3.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(path.resolve(path.dirname("")), "index.html"));
+});
+
+app.get("/graph", async (req, res) => {
+  let amountInBNB = "2000000000000";
+  // await initiateApp(amountInBNB);
+  const graph = await initiateApp(amountInBNB);
+  const filteredData = graph.filter((f) => f !== null);
+  res.json(filteredData);
+});
 
 app.listen(PORT, async () => {
   /**
@@ -21,9 +37,6 @@ app.listen(PORT, async () => {
    *
    *
    */
-  let amountInBNB = "2000000000000";
-  // await initiateApp(amountInBNB);
-  const graph = setInterval(async () => await initiateApp(amountInBNB), 360000);
 
   //graph has values
   //x - axis - block
